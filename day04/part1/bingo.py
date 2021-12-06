d3185679b81cc7764059611422c81e7090b9f91e
kavisherlock <kavishrmunjal@gmail.com>
2021-12-05 18:56:48 -0600

    Completed days 4 and 5

---
+++ day04/part1/bingo.py
@@ -0,0 +1,60 @@
+f = open("../input", "r")
+
+contents = f.readlines()
+
+drawn_numbers = contents[0].split(',')
+
+boards = []
+cur_board = []
+for i in range(2, len(contents)):
+  line = contents[i].strip()
+  if len(line) == 0:
+    boards.append(cur_board)
+    cur_board = []
+    continue
+
+  cur_board += list(filter(lambda n : len(n.strip()) > 0, line.split(' ')))
+
+def get_position_on_board(board, number_drawn):
+  for i in range(0, len(board)):
+    if board[i] == number_drawn:
+      return i
+  
+  return -1
+
+def check_board_for_bingo(board):
+  for i in range(0, 5):
+    row_check = 0
+    for j in range(i * 5, (i + 1) * 5):
+      if board[j].endswith('D'):
+        row_check += 1
+    if row_check == 5:
+      return True
+
+  for i in range(0, 5):
+    col_check = 0
+    for j in range(0, 5):
+      if board[j * 5 + i].endswith('D'):
+        col_check += 1
+    if col_check == 5:
+      return True
+  
+  return False
+
+def bingo(boards, drawn_numbers):
+  for number in drawn_numbers:
+    for board in boards:
+      pos = get_position_on_board(board, number)
+      if pos == -1: continue
+
+      board[pos] += 'D'
+      if check_board_for_bingo(board):
+        unmarked_total = 0
+        for n in board:
+          if not n.endswith('D'):
+            unmarked_total += int(n)
+        
+        print(unmarked_total * int(number))
+        return
+
+bingo(boards, drawn_numbers)
---
+++ day05/part1/pipes.py
@@ -0,0 +1,34 @@
+f = open("../input", "r")
+
+contents = f.readlines()
+
+DIMENSION = 1000
+
+ocean_floor = []
+for i in range(DIMENSION):
+  ocean_floor.append([0] * DIMENSION)
+
+for pipe in contents:
+  if len(pipe.strip()) == 0: continue
+  pipe_ends = pipe.strip().split(' -> ')
+  end1 = pipe_ends[0].split(',')
+  x1 = int(end1[0])
+  y1 = int(end1[1])
+  end2 = pipe_ends[1].split(',')
+  x2 = int(end2[0])
+  y2 = int(end2[1])
+
+  if x1 == x2 or y1 == y2:
+    if x1 == x2:
+      for y in range(min(y1, y2), max(y1, y2) + 1):
+        ocean_floor[y][x1] += 1
+    if y1 == y2:
+      for x in range(min(x1, x2), max(x1, x2) + 1):
+        ocean_floor[y1][x] += 1
+
+n_overlaps = 0
+for row in ocean_floor:
+  for point in row:
+    if point > 1:
+      n_overlaps += 1
+print(n_overlaps)
